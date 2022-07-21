2ac36c532405c2f7beb47d923592e28eb1e54ceb
Harold Zarco <harold.jzarco@gmail.com>
2022-07-20 22:38:38 -0600

    edit home view, desktop

---
+++ assets/js/slider.js
@@ -0,0 +1,17 @@
+$('.slider-nav').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    dots: true,
+    focusOnSelect: true,
+    /* responsive: [
+        {
+            breakpoint: 1101,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true
+            }
+        },
+    ], */
+});
