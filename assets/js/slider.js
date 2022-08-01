2a46fc0d75dfe5a527c624e091da1449aa52bb54
Harold Zarco <harold.jzarco@gmail.com>
2022-07-31 22:41:32 -0600

    edit sliders, mobile

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,5 +1,6 @@
 /* Slider Cards */
 var cantidad = 0;
+var cont = 0;
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
     spaceBetween: 30,
@@ -12,27 +13,32 @@ var swiper = new Swiper(".mySwiper", {
         0: {
             spaceBetween: 350,
             loop: true,
-            initialSlide: 0, //this one accept a number according to docs
-            slidesPerView:2, //number or 'auto'
+            initialSlide: 0,
+            slidesPerView: 2,
+
         },
         768: {
             slidesPerView: 2,
             spaceBetween: 10,
         },
     },
-});
-
-/* Slider work */
-var cantidad = 0;
-var swiper = new Swiper(".mySwiper_work", {
-    slidesPerView: 2,
-    grid: {
-        rows: 2
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+    on: {
+        slideChange: function () {
+            cont++
+            $(window).resize(function () {
+                var $win = $(this);
+                if ($win.width() <= 768) {
+                    if (cont === 2 && $(window).width() <= 768) {
+                        $(".swiper-slide>div:first-child").css("margin-left", "20px");
+                        if (swiper.activeIndex < swiper.previousIndex) {
+                            console.log('a')
+                            swiper.slideTo(2);
+                        }
+                    }
+                }
+            }).resize();
+        }
+    }
 });
 
 /* Slider Reviews */
@@ -47,9 +53,21 @@ $('.slider-nav').slick({
             settings: {
                 slidesToShow: 1,
                 slidesToScroll: 1,
+                infinitive: false,
                 infinite: true,
+                loop: false,
                 dots: true
             }
         },
     ],
+});
+
+var contr = 0;
+
+$(".slider-nav").on("afterChange", function (){
+    contr++;
+    if(cont === 1 && $(window).width() <= 768){
+        $(".reviews__cards").css("margin-left", '0rem');
+        $(".slick-slide").css("margin-right", '0');
+    }
 });
